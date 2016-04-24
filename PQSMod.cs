d6de9228cddf70e9eb55983ef2127db39d089113
Thomas <dorian@tmsp.ga>
2016-04-24 14:43:00 +0200

    Added base PQSMod class

---
+++ PQSMod.cs
@@ -0,0 +1,44 @@
+﻿/**
+ * libpqsmods - A standalone implementation of KSP's PQSMods
+ * Copyright (c) Thomas P. 2016
+ * Licensed under the terms of the MIT license
+ */
+
+using System;
+
+namespace PQS
+{
+    /// <summary>
+    /// Implementation of the base PQSMod type. Does nothing on it's own.
+    /// </summary>
+    public abstract class PQSMod
+    {
+        /// <summary>
+        /// Whether the mod should be taken into account for building something
+        /// </summary>
+        public Boolean modEnabled = true;
+
+        /// <summary>
+        /// The position of the mod in the build queue
+        /// </summary>
+        public Int32 order = 100;
+
+        #region Virtuals
+        public virtual void OnSetup() { }
+        public virtual void OnPostSetup() { }
+        public virtual void OnSphereReset() { }
+        public virtual void OnSphereActive() { }
+        public virtual void OnSphereInactive() { }
+        public virtual Boolean OnSphereStart() { return false; }
+        public virtual void OnSphereStarted() { }
+        public virtual void OnSphereTransformUpdate() { }
+        public virtual void OnPreUpdate() { }
+        public virtual void OnUpdateFinished() { }
+        public virtual void OnVertexBuild(VertexBuildData data) { }
+        public virtual void OnVertexBuildHeight(VertexBuildData data) { }
+        public virtual Double GetVertexMaxHeight() { return 0d; }
+        public virtual double GetVertexMinHeight() { return 0d; }
+        public virtual void OnMeshBuild() { }
+        #endregion
+    }
+}
---
+++ VertexBuildData.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * libpqsmods - A standalone implementation of KSP's PQSMods
+ * Copyright (c) Thomas P. 2016
+ * Licensed under the terms of the MIT license
+ */
+
+using System;
+using System.Drawing;
+using XnaGeometry;
+
+namespace PQS
+{
+    /// <summary>
+    /// A structure that defines the exact position that the <see cref="PQSMod"/>
+    /// should build. Some parameters have been stripped from the KSP
+    /// version because they can't be used without a complete PQS.
+    /// </summary>
+    public class VertexBuildData
+    {
+        public Vector3 globalV { get; set; }
+        public Vector3 directionFromCenter { get; set; }
+        public Vector3 directionD { get; set; }
+        public Vector3 directionXZ { get; set; }
+        public Int32 vertIndex { get; set; }
+        public Double vertHeight { get; set; }
+        public Color vertColor { get; set; }
+        public Double u { get; set; }
+        public Double v { get; set; }
+        public Double u2 { get; set; }
+        public Double v2 { get; set; }
+        public Double gnomonicU { get; set; }
+        public Double gnomonicV { get; set; }
+        public Boolean allowScatter { get; set; }
+        public Double latitude { get; set; }
+        public Double longitude { get; set; }
+    }
+}

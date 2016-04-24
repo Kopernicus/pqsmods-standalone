8f17757b841d265c0c11c5eb1d07d4e059c9cd53
Thomas <dorian@tmsp.ga>
2016-04-24 13:30:13 +0200

    Added Visual Studio solution

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("libpqsmods")]
+[assembly: AssemblyDescription("Standalone implementation of some PQSMods")]
+[assembly: AssemblyProduct("libpqsmods")]
+[assembly: AssemblyCopyright("Copyright © Thomas P. 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c159fa9e-6fd7-4ffb-bad3-ed2ebf754b47")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

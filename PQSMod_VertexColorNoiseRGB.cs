f945b5f0bc8ffc9caa028e6ce563c8d02e3cb586
Thomas <dorian@tmsp.ga>
2016-05-01 12:56:51 +0200

    Added VertexColorNoiseRGB

---
+++ PQSMod_VertexColorNoiseRGB.cs
@@ -0,0 +1,95 @@
+/**
+ * libpqsmods - A standalone implementation of KSP's PQSMods
+ * Copyright (c) Thomas P. 2016
+ * Licensed under the terms of the MIT license
+ */
+
+using LibNoise;
+using LibNoise.Generator;
+using System;
+using PQS.Unity;
+
+namespace PQS
+{
+    /// <summary>
+    /// PQSMod that applies color based on a noise generator with a blend value added to each channel
+    /// </summary>
+    /// <seealso cref="PQSMod" />
+    public class PQSMod_VertexColorNoiseRGB : PQSMod
+    {
+        public enum NoiseType
+        {
+            Perlin,
+            RiggedMultifractal,
+            Billow
+        }
+
+        /// <summary>
+        /// The type of the noise that gets used.
+        /// </summary>
+        public NoiseType noiseType;
+
+        /// <summary>
+        /// The red blend value
+        /// </summary>
+        public float rBlend;
+
+        /// <summary>
+        /// The green blend value
+        /// </summary>
+        public float gBlend;
+
+        /// <summary>
+        /// The blue blend value
+        /// </summary>
+        public float bBlend;
+
+        #region Noise Values
+        public float blend;
+        public int seed;
+        public float frequency;
+        public float lacunarity;
+        public float persistance;
+        public int octaves;
+        public QualityMode mode;
+        private ModuleBase noiseMap;
+        #endregion
+
+        /// <summary>
+        /// The color that gets created from the noise
+        /// </summary>
+        private Color c;
+
+        /// <summary>
+        /// Initializes the base mod
+        /// </summary>
+        public override void OnSetup()
+        {
+            switch (noiseType)
+            {
+                case NoiseType.Perlin:
+                    noiseMap = new Perlin(frequency, lacunarity, persistance, octaves, seed, mode);
+                    break;
+                case NoiseType.Billow:
+                    noiseMap = new Billow(frequency, lacunarity, persistance, octaves, seed, mode);
+                    break;
+                case NoiseType.RiggedMultifractal:
+                    noiseMap = new RidgedMultifractal(frequency, lacunarity, octaves, seed, mode);
+                    break;
+                default:
+                    throw new ArgumentException("Noise Type seems to be something undefineable. You are a scrub.", nameof(noiseType));
+            }
+            c = Color.white;
+        }
+
+        /// <summary>
+        /// Called when the parent sphere builds it's height
+        /// </summary>
+        public override void OnVertexBuildHeight(VertexBuildData data)
+        {
+            float h = (float)((noiseMap.GetValue(data.directionFromCenter) + 1.0) * 0.5);
+            c.r = h * rBlend; c.g = h * gBlend; c.b = h * bBlend;
+            data.vertColor = Color.Lerp(data.vertColor, c, blend);
+        }
+    }
+}

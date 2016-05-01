1bf8b055824ae81662b125542a6e19820ee49279
Thomas <dorian@tmsp.ga>
2016-05-01 13:03:44 +0200

    Added VertexColorSolid

---
+++ PQSMod_VertexColorSolid.cs
@@ -0,0 +1,35 @@
+/**
+ * libpqsmods - A standalone implementation of KSP's PQSMods
+ * Copyright (c) Thomas P. 2016
+ * Licensed under the terms of the MIT license
+ */
+
+using PQS.Unity;
+
+namespace PQS
+{
+    /// <summary>
+    /// A PQSMod that lerps a solid color into the pqs color
+    /// </summary>
+    /// <seealso cref="PQSMod" />
+    public class PQSMod_VertexColorSolid : PQSMod
+    {
+        /// <summary>
+        /// The color the mod applies to the surface
+        /// </summary>
+        public Color color;
+
+        /// <summary>
+        /// How much of the color should end up on the surface
+        /// </summary>
+        public float blend;
+
+        /// <summary>
+        /// Called when the parent sphere builds it's height
+        /// </summary>
+        public override void OnVertexBuild(VertexBuildData data)
+        {
+            data.vertColor = Color.Lerp(data.vertColor, color, blend);
+        }
+    }
+}
